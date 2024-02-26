f54f71c822005d308b381c624a125d4f92161b76
kimberlykeilymh <kimberlymendoza154@gmail.com>
2024-02-25 23:48:06 -0300

    feat: add database seed

---
+++ db/seed.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative '../lib/database_connection'
+
+db = DatabaseConnection.db_client
+db.drop_table?(:products, :users)
+DatabaseConnection.create_tables
+
+require_relative '../app/models/user'
+require_relative '../app/models/product'
+
+user_a = Models::User.create(username: 'kmendoza', password_digest: 'pFwwG.gnk!')
+user_b = Models::User.create(username: 'kkeily', password_digest: 'zetyu8;ng')
+
+Models::Product.create(name: 'Product A', price: 1500.99, user_id: user_a.id)
+Models::Product.create(name: 'Product B', price: 799.99, user_id: user_a.id)
+Models::Product.create(name: 'Product C', price: 2500.99, user_id: user_a.id)
+Models::Product.create(name: 'Product D', price: 12150.99, user_id: user_a.id)
+Models::Product.create(name: 'Product E', price: 1500.99, user_id: user_a.id)
+Models::Product.create(name: 'Product KK-1', price: 25.60, user_id: user_b.id)
